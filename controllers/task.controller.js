cf0f1a16bf399a2c3698d0485968a18d097fe0da
ChaavieNandakishor <chaavienandakishor@gmail.com>
2023-12-26 18:00:25 +0530

    Add : getTasksByPriority

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -6,10 +6,10 @@ const createNewTask = async (req, res) => {
   try {
     const { title, description, dueDate, priority, owner } = req.body;
     if (!title || !dueDate) {
-      const error = "title and duedate required";
-      logger.error(error);
+      const errorMessage = "title and duedate required";
+      logger.error(errorMessage);
       return res.status(400).json({
-        error: error,
+        error: errorMessage,
       });
     }
 
@@ -34,11 +34,35 @@ const createNewTask = async (req, res) => {
 
 const getAllTasks = async (req, res) => {
   try {
-    const tasks =await Task.find();
-    res.send(tasks)
+    const tasks = await Task.find().select(
+      "title description dueDate completed priority"
+    );
+    res.send(tasks);
   } catch (err) {}
 };
-const getTasksByPriority = async (req, res) => {};
+const getTasksByPriority = async (req, res) => {
+  try {
+    const { priority } = req.body;
+    if (!priority) {
+      const errorMessage = "priority required";
+      logger.error(errorMessage);
+      return res.status(400).json({ success: false, message: errorMessage });
+    }
+    const response = await Task.find({
+      priority: { $regex: priority, $options: "i" },
+    });
+    res.status(200).json({
+      success: true,
+      data: response,
+    });
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 const getTasksByID = async (req, res) => {};
 const updateTask = async (req, res) => {};
 const deleteTask = async (req, res) => {};
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -6,5 +6,6 @@ const taskControllers=require('../controllers/task.controller')
 router.post('/newtask',taskControllers.createNewTask)
 router.post('/updatetask',taskControllers.updateTask)
 router.get('/getalltasks',taskControllers.getAllTasks)
+router.post('/getbypriority',taskControllers.getTasksByPriority)
 
 module.exports=router;
